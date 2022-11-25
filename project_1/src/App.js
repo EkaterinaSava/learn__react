14fd439c5d0d5105a042e9713d2f4b1016d0b1ac
Ekaterina Kozlova <e.kozlova@devcaz.com>
2022-11-25 17:24:49 +0100

    Add MaterializeCSS lib & Add Header + Footer & Add small styling

--- project_1/src/App.js
+++ project_1/src/App.js
@@ -1,12 +1,14 @@
+import { Header } from './layout/Header'
+import { Footer } from './layout/Footer'
+import { Main } from './layout/Main'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ project_1/src/layout/Main.jsx
@@ -0,0 +1,11 @@
+function Main() {
+  return (
+    <main className="page-main">
+      <div className="container">
+        Main
+      </div>
+    </main>
+  )
+}
+
+export { Main };
