50435ae76281fcea35fe7992860f6d42639a167e
Ekaterina Kozlova <e.kozlova@devcaz.com>
2022-11-28 16:57:41 +0100

    Add api key to env.local & Add real link for footer

--- project_1/src/layout/Footer.jsx
+++ project_1/src/layout/Footer.jsx
@@ -1,10 +1,17 @@
 function Footer() {
   return (
-    <footer className="page-footer green lighten-2">
+    <footer className="page-footer green lighten-1">
     <div className="footer-copyright">
       <div className="container">
-      © {new Date().getFullYear()} Copyright Text
-      <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+      © {new Date().getFullYear()} Copyright / Ekaterina Sava
+      <a
+        className="grey-text text-lighten-4 right"
+        href="https://github.com/EkaterinaSava"
+        rel="noreferrer" 
+        target="_blank"
+      >
+          My Github
+      </a>
       </div>
     </div>
   </footer>
