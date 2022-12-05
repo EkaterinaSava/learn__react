59641bf7e1fa18e90f3c81b63e759cf04eaa29cf
Ekaterina Sava <ekaterinasava@gmail.com>
2022-12-05 11:41:09 +0100

    Project #3: Create project by CRA & Add base routing

---
+++ project_3/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+function Footer() {
+  return (
+    <footer className="page-footer teal lighten-2">
+    <div className="footer-copyright">
+      <div className="container">
+      © {new Date().getFullYear()} Copyright / Ekaterina Sava
+      <a
+        className="grey-text text-lighten-4 right"
+        href="https://github.com/EkaterinaSava"
+        rel="noreferrer" 
+        target="_blank"
+      >
+          My Github
+      </a>
+      </div>
+    </div>
+  </footer>
+  )
+}
+
+export { Footer };
---
+++ project_3/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+function NotFound() {
+  return (
+    <div>
+      <h4 style={{textAlign: 'center'}}>Sorry, but page not found.</h4>
+    </div>
+  );
+}
+
+export { NotFound };
