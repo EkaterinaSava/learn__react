ff6a653d202640f5928b4d183655e94e318b10c6
Ekaterina Sava <ekaterinasava@gmail.com>
2022-12-07 14:49:08 +0100

    Project #4: Add case3 example - custom hook

---
+++ project_4/src/case3.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+function useCounter(initialValue = 0, delta = 1) {
+  const [count, setCount] = useState(initialValue);
+
+  const increment = () => {
+    setCount(prevValue => prevValue + delta);
+  }
+
+  const decrement = () => {
+    setCount(prevValue => prevValue - delta);
+  }
+
+  return [count, increment, decrement];
+}
+
+function YearCounter({ initialValue }) {
+  const [count, inc, dec] = useCounter(initialValue, 1);
+
+  return (
+    <div className="year-counter">
+      <p>Year:</p>
+      <div>
+        <button onClick={dec}>{'<<'}</button>
+        <span style={{ margin: 5 }}>{count}</span>
+        <button onClick={inc}>{'>>'}</button>
+      </div>
+    </div>
+  )
+}
+
+function DecadesCounter({ initialValue }) {
+  const [count, inc] = useCounter(initialValue, 10);
+
+  return (
+    <div className="decade-counter">
+      <p>Decade: <button onClick={inc}>{count}'s</button></p>
+    </div>
+  )
+}
+
+export default function ExampleCounters() {
+  return (
+    <div>
+      <YearCounter initialValue={1970} />
+      <DecadesCounter initialValue={1970} />
+    </div>
+  )
+}
--- project_4/src/index.js
+++ project_4/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Clicker from './case1';
 import FormExample from './case2';
+import ExampleCounters from './case3';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -17,6 +18,11 @@ root.render(
     <div className="component">
       <h3 className="component__heading">Example #2: Form with state</h3>
       <FormExample />
+    </div>
+
+    <div className="component">
+      <h3 className="component__heading">Example #3: Counters with Custom Hooks</h3>
+      <ExampleCounters />
     </div>
 
   </React.StrictMode>
