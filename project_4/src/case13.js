07b253863109594f8c24c47cc317e9245b6f58f0
Ekaterina Sava <ekaterinasava@gmail.com>
2022-12-21 16:47:28 +0100

    Project #4: Upd case13 example - create folder for components

--- project_4/src/case13.js
+++ project_4/src/case13.js
@@ -1,8 +1,8 @@
-import { ThemeProvider } from './components/ThemeProvider';
-import { ThemeToolbar } from './components/ThemeToolbar';
-import { Label } from './components/Label';
-import { ThemeLabel } from './components/ThemeLabel';
-import { UserPanel } from './components/UserPanel';
+import { ThemeProvider } from './components/Case13/ThemeProvider';
+import { ThemeToolbar } from './components/Case13/ThemeToolbar';
+import { Label } from './components/Case13/Label';
+import { ThemeLabel } from './components/Case13/ThemeLabel';
+import { UserPanel } from './components/Case13/UserPanel';
 
 export function ContextExample() {
   return (
