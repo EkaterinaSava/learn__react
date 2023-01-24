a98b4a334cf83edc3f1c6e73c10a1e791755ed14
Ekaterina Sava <ekaterinasava@gmail.com>
2023-01-24 11:46:00 +0100

    Project #5: Add filter by year

--- project_5/src/components/Costs/Costs.js
+++ project_5/src/components/Costs/Costs.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
+import CostsFilter from './CostsFilter';
 import CostItem from './CostItem';
 import Card from '../UI/Card';
 
 function Costs(props) {
+  const [selectedYear, setSelectedYear] = useState('2023');
+
+  const yearChangeHandler = (year) => {
+    setSelectedYear(year);
+  }
+
   return (
     <Card className="costs">
+      <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler}/>
+
       <CostItem
         date={props.costs[0].date}
         description={props.costs[0].description}
